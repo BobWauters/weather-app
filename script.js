4eb93f8fdf877c6872d07bc84cceb7d87c25ebd0
BobW <biz.wautersbob@gmail.com>
2021-06-29 16:27:03 +0200

    Fixed var/let nameValue

--- script.js
+++ script.js
@@ -12,28 +12,40 @@
 //     Allow the user to compare the weather in two cities
 //     Use the API of https://unsplash.com/ to show a photo of the city they entered in the form.
 
-var button = document.querySelector('.button');
-var inputValue = document.querySelector('.inputValue');
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+let button = document.querySelector('.button');
+let inputValue = document.querySelector('.inputValue');
+let name = document.querySelector('#name');
+let desc = document.querySelector('.desc');
+let temp = document.querySelector('.temp');
+let firstDay;
+let secondDay;
+let thirdDay;
+let fourthDay;
+let fifthDay;
+
+
 
 
 //API TRY 2
 button.addEventListener('click', function (){
     fetch ('https://api.openweathermap.org/data/2.5/forecast?q=' + inputValue.value +'&appid=4ce0604fe97650f47963ef28c9f566d5')
         .then (response => response.json())
+        //.then (data => console.log(data))
 
 
 
        .then(data => {
-            let nameValue = data[0] ;
-
+            let nameValue = data.list[0].weather[0].main;
+            let tempValue = data.list[0].main.temp;
+          //  console.log (Math.round(data.list[2].main.temp - 273.15) + '&degC')
+            console.log(tempValue);
+            console.log(nameValue);
+            console.log(name)
 
 
 
             name.innerHTML= nameValue;
-
+            temp.innerHTML= tempValue;
 
 
 
@@ -41,9 +53,9 @@ button.addEventListener('click', function (){
 
 
 
-        .catch(err => alert("An Error Occured"))
-
-
+        //.catch(err => alert("An Error Occured"))
+        //.catch(err) {console.log(err);}
+        .catch(err => {console.log(err)});
 })
 
 
